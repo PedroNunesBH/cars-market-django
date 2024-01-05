30d43c7f9f76694879a5c21891cbeef432a665d6
Pedro Nunes <pedrohenriquestore99@gmail.com>
2024-01-05 16:33:23 -0300

    Alteracao do campo brand de car para FK e alteracoes no registro de Brand

--- cars/admin.py
+++ cars/admin.py
@@ -4,15 +4,15 @@ from .models import Car, Brand
 # Register your models here.
 
 
-class CarAdmin(admin.ModelAdmin):
+class CarAdmin(admin.ModelAdmin):  # Criacao de uma classe para registro do model Car no admin
     list_display = ('model', 'brand', 'factory_year', 'model_year', 'value')  # Campos que serão mostrados
     search_fields = ('model', 'brand')  # Campo(s) que serão usados para fazer busca
 
 
-class BrandAdmin(admin.ModelAdmin):
-    list_display = ('brand_name',)
-    search_fields = ('brand_name',)
+class BrandAdmin(admin.ModelAdmin):  # Criacao da classe responsavel pelo registro do model Brand no admin
+    list_display = ('name',)
+    search_fields = ('name',)
 
 
-admin.site.register(Car, CarAdmin)
-admin.site.register(Brand, BrandAdmin)
+admin.site.register(Car, CarAdmin)  # Registro de Car no admin
+admin.site.register(Brand, BrandAdmin)  # Registro de Brand no admin
--- cars/models.py
+++ cars/models.py
@@ -1,21 +1,21 @@
 from django.db import models
 
 
+class Brand(models.Model):  # Criacao de um banco de dados para marcas do carro
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200, default='')
+
+    def __str__(self):
+        return self.name  # Modificando a representação dos objetos para o atributo name
+
+
 class Car(models.Model):
     id = models.AutoField(primary_key=True)
     model = models.CharField(max_length=200)
-    brand = models.CharField(max_length=200)
+    brand = models.ForeignKey(Brand, on_delete=models.PROTECT, related_name='car_brand')  # Criando um campo de FK ligando a tabela Brand
     factory_year = models.IntegerField(blank=True, null=True)
     model_year = models.IntegerField(blank=True, null=True)
     value = models.FloatField(blank=True, null=True)
 
     def __str__(self):
         return self.model  # Configurando para o objeto ser representado pelo seu atributo model
-
-
-class Brand(models.Model):
-    id = models.AutoField(primary_key=True)
-    brand_name = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.brand_name
