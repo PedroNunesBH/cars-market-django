b6b6e9303802cdfa6a40598d783c91c6b4d5bc29
Pedro Nunes <pedrohenriquestore99@gmail.com>
2024-01-08 15:43:45 -0300

    Criacao de html e css para a url cars e algumas estilizacoes iniciais

--- cars/views.py
+++ cars/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from .models import Car
 
 
 def cars_view(request):
-    return render(request, 'cars.html')
+    cars = Car.objects.all()  # Retorna todos os objetos do modelo Car
+
+    return render(
+        request,
+        'cars.html',
+        {'cars': cars})
+
