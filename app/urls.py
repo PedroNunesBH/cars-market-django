98a803ca1b87ea8d81182bf985b7c52e2955cf46
Pedro Nunes <pedrohenriquestore99@gmail.com>
2024-01-12 10:48:55 -0300

    Criacao da login view e autenticacao do usuario e alteracao do redirect do cadastro do usuario para a pagina de login

--- app/urls.py
+++ app/urls.py
@@ -3,11 +3,12 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from cars.views import cars_view, user_register_new_car
-from users.views import create_user
+from users.views import create_user, login_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cars/', cars_view, name="cars_list"),  # URL e a view responsavel
-    path('user_register_car', user_register_new_car, name="u_register_car"),
-    path('create_user', create_user, name='create_user')
+    path('user_register_car/', user_register_new_car, name="u_register_car"),
+    path('create_user/', create_user, name='create_user'),
+    path('login/', login_view, name='login_page'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- users/views.py
+++ users/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 from django.views.generic import TemplateView
 from cars.models import Brand
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 
 def create_user(request):
@@ -10,7 +11,23 @@ def create_user(request):
         user_creation_form = UserCreationForm(request.POST)
         if user_creation_form.is_valid():
             user_creation_form.save()
-            return redirect('cars_list')
+            return redirect('login_page')  # Redireciona para a pagina de login
     else:
         user_creation_form = UserCreationForm()
     return render(request, 'create_user.html', {'car_brands': car_brands, 'user_creation_form': user_creation_form})
+
+
+def login_view(request):
+    car_brands = Brand.objects.all()
+    if request.method == "POST":
+        username = request.POST['username']  # Captura o valor de username
+        password = request.POST['password']  # Captura o valor de password
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)  # Realiza o login
+            return redirect('cars_list')  # Redireciona o usuario para a pagina de name cars_list
+        else:
+            auth_form = AuthenticationForm()
+    else:
+        auth_form = AuthenticationForm()
+    return render(request, "login.html", {'car_objects': car_brands, 'auth_form': auth_form})
